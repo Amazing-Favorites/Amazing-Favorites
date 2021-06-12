e546f0335627882f324924eed145bb1ecfda4db2
Newbe36524 <newbe36524@qq.com>
2021-06-12 22:13:02 +0800

    Tags Enhancement (#45)
    * close #44
    
    * close #20
    
    * close #30

--- src/Newbe.BookmarkManager/Services/Job/Impl/SyncBookmarkJob.cs
+++ src/Newbe.BookmarkManager/Services/Job/Impl/SyncBookmarkJob.cs
@@ -27,7 +27,7 @@ namespace Newbe.BookmarkManager.Services
         {
             await _bookmarksApi.OnRemoved.AddListener(async (s, info) =>
             {
-                var nodes = GetAllChildren(new []{info.Node});
+                var nodes = GetAllChildren(new[] {info.Node});
                 foreach (var bookmarkTreeNode in nodes)
                 {
                     await _bkManager.DeleteAsync(bookmarkTreeNode.Url);
@@ -50,7 +50,20 @@ namespace Newbe.BookmarkManager.Services
                 if (!string.IsNullOrWhiteSpace(node.Url) &&
                     !string.IsNullOrWhiteSpace(node.Title))
                 {
-                    await _bkManager.AppendBookmarksAsync(new[] {node});
+                    var tags = new HashSet<string>();
+                    var nodes = _bookmarksApi.GetAllParentAsync(node.ParentId);
+                    await foreach (var parentNode in nodes)
+                    {
+                        if (!string.IsNullOrWhiteSpace(parentNode.Title)
+                            && !Consts.IsReservedBookmarkFolder(parentNode.Title))
+                        {
+                            tags.Add(parentNode.Title);
+                        }
+                    }
+                    await _bkManager.AppendBookmarksAsync(new[] {new BookmarkNode(node)
+                    {
+                        Tags = tags.ToList()
+                    }});
                 }
 
                 _logger.LogInformation("Bookmark data reload since item added");
@@ -66,40 +79,56 @@ namespace Newbe.BookmarkManager.Services
             });
         }
 
-        private async ValueTask<List<BookmarkTreeNode>> GetAllBookmarkAsync()
+        private async ValueTask<List<BookmarkNode>> GetAllBookmarkAsync()
         {
             var bookmarkTreeNodes = await _bookmarksApi.GetTree();
             if (bookmarkTreeNodes == null)
             {
-                return new List<BookmarkTreeNode>();
+                return new List<BookmarkNode>();
             }
 
             return GetAllChildren(bookmarkTreeNodes);
         }
 
-        private static List<BookmarkTreeNode> GetAllChildren(IEnumerable<BookmarkTreeNode> bookmarkTreeNodes)
+        private static List<BookmarkNode> GetAllChildren(IEnumerable<BookmarkTreeNode> bookmarkTreeNodes)
         {
-            var queue = new Queue<BookmarkTreeNode>(bookmarkTreeNodes);
-            var result = new List<BookmarkTreeNode>();
-            while (queue.TryDequeue(out var node))
+            var queue = new Queue<BkItem>(bookmarkTreeNodes.Select(x => new BkItem(x, new BookmarkNode(x)
             {
+                Tags = new List<string>()
+            })));
+            var result = new List<BookmarkNode>();
+            while (queue.TryDequeue(out var item))
+            {
+                var (node, bookmarkNode) = item;
                 if (!string.IsNullOrWhiteSpace(node.Url) &&
                     !string.IsNullOrWhiteSpace(node.Title) &&
                     node.Unmodifiable != BookmarkTreeNodeUnmodifiable.Managed)
                 {
-                    result.Add(node);
+                    result.Add(bookmarkNode);
                 }
 
                 if (node.Children != null)
                 {
                     foreach (var child in node.Children)
                     {
-                        queue.Enqueue(child);
+                        var tags = new List<string>(bookmarkNode.Tags);
+                        if (!string.IsNullOrWhiteSpace(node.Title)
+                            && !Consts.IsReservedBookmarkFolder(node.Title))
+                        {
+                            tags.Add(node.Title);
+                        }
+
+                        queue.Enqueue(new BkItem(child, new BookmarkNode(child)
+                        {
+                            Tags = tags
+                        }));
                     }
                 }
             }
 
             return result;
         }
+
+        public record BkItem(BookmarkTreeNode TreeNode, BookmarkNode Node);
     }
 }
---
+++ src/Newbe.BookmarkManager/Services/Services/BookmarkApiExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebExtensions.Net.Bookmarks;
+
+namespace Newbe.BookmarkManager.Services
+{
+    public static class BookmarkApiExtensions
+    {
+        public static async IAsyncEnumerable<BookmarkTreeNode> GetAllParentAsync(this IBookmarksApi bookmarksApi,
+            string id)
+        {
+            var node = await bookmarksApi.Get(id);
+            var currentNode = node?.FirstOrDefault();
+            while (currentNode != null)
+            {
+                yield return currentNode;
+                var nodes = await bookmarksApi.Get(currentNode.ParentId);
+                currentNode = nodes?.FirstOrDefault();
+            }
+        }
+    }
+}
