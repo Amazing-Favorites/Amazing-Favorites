5ee82bf512c5f238001fb891e592e4acc90fa1f4
newbe36524 <newbe36524@qq.com>
2021-06-22 23:04:14 +0800

    Optimize input box to make user input more smooth

--- src/Newbe.BookmarkManager/wwwroot/content/manager_keyboard.js
+++ src/Newbe.BookmarkManager/wwwroot/content/manager_keyboard.js
@@ -12,4 +12,14 @@
             await GLOBAL.DotNetReference.invokeMethodAsync('OnReceivedCommand', c);
         });
     }, 5000);
+
+    document.onkeydown = async function (event) {
+        const e = event || window.event || arguments.callee.caller.arguments[0];
+        await GLOBAL.DotNetReference.invokeMethodAsync('OnSearchInputKeydown', e.code, e.altKey);
+    };
+
+    document.onkeyup = async function (event) {
+        const e = event || window.event || arguments.callee.caller.arguments[0];
+        await GLOBAL.DotNetReference.invokeMethodAsync('OnSearchInputKeyup', e.code, e.altKey);
+    };
 })();
