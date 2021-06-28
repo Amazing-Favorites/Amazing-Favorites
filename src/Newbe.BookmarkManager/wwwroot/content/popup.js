6f90e4aea95ac88056044e594c37e26b9d407128
Newbe36524 <newbe36524@qq.com>
2021-06-28 22:58:22 +0800

    Support to add bookmark immediately when clicking icon (#64)

---
+++ src/Newbe.BookmarkManager/wwwroot/content/popup.js
@@ -0,0 +1,64 @@
+﻿(async () => {
+    const folderTitle = "Amazing Favorites"
+    const managerTabTitle = "Amazing Favorites";
+    const tabs = await browser.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+    if (tabs.length > 0 && tabs[0].url && tabs[0].title) {
+        const tab = tabs[0];
+        const bks = await browser.bookmarks.search({
+            url: tab.url
+        })
+        let bkNode = null;
+        if (bks.length) {
+            bkNode = bks[0];
+            console.log("bookmark already exists");
+        } else {
+            // create amazing favorites bookmark folder
+            const nodes = await browser.bookmarks.search({
+                title: folderTitle
+            })
+            let folderNode = null;
+            let createFolder = false;
+            if (nodes.length) {
+                folderNode = nodes[0]
+                if (folderNode.type !== "folder") {
+                    createFolder = true
+                }
+            } else {
+                createFolder = true
+            }
+            if (createFolder) {
+                folderNode = await browser.bookmarks.create({
+                    title: folderTitle
+                })
+            }
+
+            // create bookmark
+            bkNode = await browser.bookmarks.create({
+                title: tab.title,
+                url: tab.url,
+                parentId: folderNode.id
+            })
+            console.log("new bookmark created");
+        }
+        const managerTabs = await browser.tabs.query({
+            title: managerTabTitle
+        });
+        if (managerTabs.length > 0) {
+            await browser.tabs.update(managerTabs[0].id, {
+                active: true
+            });
+            await browser.runtime.sendMessage({
+                title: bkNode.title,
+                url: bkNode.url,
+                tabId: tab.id
+            });
+        } else {
+            await browser.tabs.create({
+                url: "/Manager/index.html?editTabId=" + tab.id
+            });
+        }
+    }
+})();
