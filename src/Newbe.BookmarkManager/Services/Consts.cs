eaf3e4797c7a11c7e19cd2b38dd28bf304ec83ac
Z4t4r <771626206@qq.com>
2021-09-13 16:32:53 +0800

    Update magicNumber to constant

--- src/Newbe.BookmarkManager/Services/Consts.cs
+++ src/Newbe.BookmarkManager/Services/Consts.cs
@@ -18,6 +18,7 @@ namespace Newbe.BookmarkManager.Services
 
         public const int OmniboxSuggestMax = 6;
         public const int OmniboxSuggestMin = 1;
+        public const int OmniboxSuggestDefault = 3;
 
 
         public const int JwtExpiredWarningDays = 7;
