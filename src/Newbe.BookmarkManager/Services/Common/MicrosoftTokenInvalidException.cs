9472a326e00657f07d06ea4afe8032e4053a389d
Z4t4r <771626206@qq.com>
2021-09-15 16:00:16 +0800

    dotnet format

--- src/Newbe.BookmarkManager/Services/Common/MicrosoftTokenInvalidException.cs
+++ src/Newbe.BookmarkManager/Services/Common/MicrosoftTokenInvalidException.cs
@@ -2,13 +2,13 @@
 
 namespace Newbe.BookmarkManager.Services
 {
-    public class MicrosoftTokenInvalidException:Exception
+    public class MicrosoftTokenInvalidException : Exception
     {
 
-        public MicrosoftTokenInvalidException(string message):base(message)
+        public MicrosoftTokenInvalidException(string message) : base(message)
         {
-            
+
         }
-        
+
     }
 }
