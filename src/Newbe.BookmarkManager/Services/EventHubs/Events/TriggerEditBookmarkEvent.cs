1164705b56eaf9c854bef8113bab215152ea9096
newbe36524 <newbe36524@qq.com>
2021-10-18 20:00:55 +0800

    fix popup with afevent issue

--- src/Newbe.BookmarkManager/Services/EventHubs/Events/TriggerEditBookmarkEvent.cs
+++ src/Newbe.BookmarkManager/Services/EventHubs/Events/TriggerEditBookmarkEvent.cs
@@ -1,9 +1,11 @@
-﻿namespace Newbe.BookmarkManager.Services.EventHubs
+﻿using System.Text.Json.Serialization;
+
+namespace Newbe.BookmarkManager.Services.EventHubs
 {
     public record TriggerEditBookmarkEvent : IAfEvent
     {
-        public string Title { get; set; }
-        public string Url { get; set; }
-        public int TabId { get; set; }
+        [JsonPropertyName("title")] public string Title { get; set; } = null!;
+        [JsonPropertyName("url")] public string Url { get; set; } = null!;
+        [JsonPropertyName("tabId")] public int TabId { get; set; }
     }
 }
